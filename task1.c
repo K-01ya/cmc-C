e11b9bd51cb3ece0a99e0517490876a394c65ee0
K-01ya <olgaalex.kozlova@gmail.com>
2020-12-08 15:03:32 +0300

    inter

--- task1.c
+++ task1.c
@@ -11,3 +11,46 @@
 #include <math.h>
 #include <signal.h>
 
+int main(int argc, char **argv) {
+	pid_t pid;
+	int fd[2];
+	if (argc < 1) {
+		printf("too few arguments\n");
+		return 1;
+	}
+	else {
+		pipe(fd);
+		pid = fork();
+		if (pid == -1) {
+			perror("fork 1");
+		}	
+		else if (pid == 0) {
+			dup2(fd[1], 1);
+			close(fd[1]);
+			close(fd[0]);
+			execlp("sort", "sort", "-u",argv[1], NULL);
+			perror("sort");
+			exit(1);
+		}
+		else {
+			wait(NULL);
+			pid = fork();
+			if (pid == -1) {
+				perror("fork 2");
+			}   
+			else if (pid == 0) {
+				dup2(fd[0], 0);
+	            close(fd[1]);
+    	        close(fd[0]);
+				execlp("wc", "wc", "-l", argv[1], NULL);
+				perror("sort");
+				exit(1);
+			}
+			else {
+				wait(NULL);
+			}
+		}
+	}
+	return 0;
+}
+
