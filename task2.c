654a8c662d2967230b16acdd5439451d56d93173
K-01ya <olgaalex.kozlova@gmail.com>
2020-12-08 16:01:39 +0300

    inter

--- task2.c
+++ task2.c
@@ -13,11 +13,24 @@
 
 int main(int argc, char **argv) {
 	int fd = open(argv[1], O_WRONLY, 0666);
-	int c[2];
-	c[1] =  0;
+	int c;
+	int n[1024];
+	int i = 0;
 	int previous;
+	int flag = 0;
+	int flag1 = 0;
+	int flag2 = 0;
 	read(fd, c, 1);
 	while (!eof(fd)) {
-		
+		read(fd, &c, 1);
+
+		while (!eof(fd) && c != '\n') {
+			previous = c;
+			read(fd, &c, 1);
+			if (previous > c) {
+				printf
+			}
+		}
 	}
+	
 }
