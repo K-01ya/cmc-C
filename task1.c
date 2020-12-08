513d427cf0d48b2b31ec54ed3eb1fafba9345058
K-01ya <olgaalex.kozlova@gmail.com>
2020-12-08 13:59:29 +0300

    init

---
+++ task1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <signal.h>
+
